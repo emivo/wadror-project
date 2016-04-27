1f6c0e1b08af7269e6333b4991a55e2457b453f8
emivo <emil.airta@helsinki.fi>
2016-04-27 18:37:31 +0300

    Progress

---
+++ spec/factories.rb
@@ -0,0 +1,24 @@
+FactoryGirl.define do
+  factory :user do
+    username "Antti"
+    admin true
+    password "Lorem1"
+    password_confirmation "Lorem1"
+  end
+
+  factory :product do
+    name "Generic-CD"
+    description "some disc"
+    price 10.0
+    stock 10
+  end
+
+  factory :order do
+
+    paid false
+  end
+
+  factory :category do
+    name "Misc."
+  end
+end
