b71653bebefe263497741f4cfcbd98a05ab11889
emivo <emil.airta@helsinki.fi>
2016-05-13 14:26:27 +0300

    Lisää testausta

--- spec/features/categories_page_spec.rb
+++ spec/features/categories_page_spec.rb
@@ -8,9 +8,21 @@ describe "Categories page" do
   end
 
   it "should contain category that has been created" do
-    Category.create name:'cds'
+    visit new_category_path
+    fill_in('category[name]', with: 'cds')
+    click_button 'Create Category'
 
     visit categories_path
     expect(page).to have_content 'cds'
+  end
+
+  it "should change categorys name when updated" do
+      c = Category.create name: 'cds'
+      visit edit_category_path(c.id)
+      fill_in('category[name]', with: 'CDs')
+      click_button 'Update Category'
+      visit categories_path
+      expect(page).not_to have_content 'cds'
+      expect(page).to have_content 'CDs'
   end
 end
--- spec/features/orders_spec.rb
+++ spec/features/orders_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 include Helpers
 
-def add_one_cd_to_cart
+def create_product
   product = FactoryGirl.create :product
   c = Category.create name: 'cds'
   product.categories << c
+end
 
+def add_one_cd_to_cart
   visit products_path
   click_link 'Generic-CD'
   click_button 'Add to cart'
@@ -16,10 +18,21 @@ def add_one_cd_to_cart
   expect(page).to have_content 'Pay and place an order'
 end
 
+def add_cart_and_check_quantity(quantity)
+  visit products_path
+  click_link 'Generic-CD'
+  fill_in('quantity', with: 2)
+  click_button 'Add to cart'
+  click_link 'Cart'
+  expect(page).to have_content 'Generic-CD'
+  expect(page).to have_content quantity
+end
+
 describe "Orders" do
   before :each do
     FactoryGirl.create :user
     sign_in(username: "Antti", password: "Lorem1")
+    create_product
   end
 
   it "should contain newly added product" do
@@ -27,18 +40,11 @@ describe "Orders" do
   end
 
   it "should contain correct amount of added products" do
-    product = FactoryGirl.create :product
-    c = Category.create name:'cds'
-    product.categories << c
+    quantity = '2'
+    add_cart_and_check_quantity(quantity)
 
-    visit products_path
-    click_link 'Generic-CD'
-    fill_in('quantity', with: 2)
-    click_button 'Add to cart'
-    click_link 'Cart'
-    expect(page).to have_content 'Generic-CD'
-    expect(page).to have_content '2'
-
+    quantity = '4'
+    add_cart_and_check_quantity(quantity)
   end
 
   it "should change status to paid when paid" do
@@ -46,6 +52,25 @@ describe "Orders" do
     click_link 'Pay and place an order'
     expect(page).to have_content 'Paid'
     expect(page).not_to have_content 'Pay and place an order'
+  end
+
+  it "should not to be able to pay without any product" do
+    click_link 'Cart'
+    expect(page).not_to have_content 'Pay and place an order'
+    page.driver.post(pay_path(1))
+    expect(page).to have_content 'cart is empty'
+  end
+
+  it "should be able to change cart with unpaid one" do
+    add_one_cd_to_cart
+    click_link 'Sign out'
+    add_one_cd_to_cart
+    sign_in(username: "Antti", password: "Lorem1")
+    click_link 'Orders'
+    click_link '1'
+    expect(page).to have_content 'Change current cart to this order'
+    click_link 'Change current cart to this order'
+    expect(page).to have_content 'This is now your current cart'
   end
 
 end
