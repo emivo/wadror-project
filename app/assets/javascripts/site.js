97a25c298918e0ea3aa5040eb169d4620cd8b7f1
emivo <emil.airta@helsinki.fi>
2016-05-13 11:51:13 +0300

    Fiksausta angular

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,8 +1,8 @@
 /**
  * Created by emivo on 28.4.2016.
  */
-var myApp = angular.module('myApp', []);
-myApp.controller("ProductsController", function ($scope, $http) {
+angular.module('myApp', [])
+    .controller("ProductsController", function ($scope, $http) {
     $http.get('products_all.json').success( function(data, status, headers, config) {
         $scope.products = data;
     });
